e86abf8612511ae4a1961486ff7c42238c0bc9d5
sangharsh <sangharsh_iitd@yahoo.com>
2024-06-18 17:45:08 +0100

    semantic ui introduced

---
+++ src/components/LeftNav.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+    SidebarPusher,
+    SidebarPushable,
+    MenuItem,
+    Header,
+    Icon,
+    Image,
+    Menu,
+    Segment,
+    Sidebar,
+    Button,
+    Container
+} from 'semantic-ui-react'
+import Main from "./Main";
+
+const LeftNav = () =>  {
+    const items = [
+        {name: 'home'},
+        {name: 'gamepad'},
+        {name: 'camera'}
+    ];
+    return (
+        <Container>
+            <SidebarPushable as={Segment}>
+                <Sidebar
+                    as={Menu}
+                    animation='push'
+                    direction='left'
+                    icon='labeled'
+                    inverted
+                    vertical
+                    visible
+                >
+                    {items.map(item => <MenuItem as='a'>
+                        <Icon name='home' />
+                        {item.name}
+                    </MenuItem>)}
+                </Sidebar>
+                <SidebarPusher>
+                    <Segment basic>
+                        <Header as='h3'>Application Content</Header>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+
+                    </Segment>
+                    <Segment basic>
+
+                        <Main></Main>
+
+                    </Segment>
+
+                </SidebarPusher>
+            </SidebarPushable>
+        </Container>
+    )
+}
+
+export default LeftNav;
