f879ff05af731c7b3bbc89f5e64e00d69ab32436
sangharsh <sangharsh_iitd@yahoo.com>
2024-06-19 13:01:12 +0100

    charts added with cashflow

--- src/components/modules/dashboard/Dashboard.tsx
+++ src/components/modules/dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import {Header, Segment} from "semantic-ui-react";
 import React from "react";
+import CashFlow from "./CashFlow";
 
 const Dashboard = () => {
     return  <Segment basic>
         <Header as='h3' dividing>Dashboard</Header>
+        <CashFlow></CashFlow>
     </Segment>
 }
 export default Dashboard;
