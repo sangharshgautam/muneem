459f97176c55d1d558e6583d63e0008649c97294
sangharsh <sangharsh_iitd@yahoo.com>
2024-06-26 15:39:07 +0100

    muneem to moneta

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Moneta/i);
+  const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
